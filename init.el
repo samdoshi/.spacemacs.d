2df4408f22557e3e5549fefd34b7e1b9759a2ae0
Sam Doshi <sam@metal-fish.co.uk>
2016-01-07 17:22:10 +0000

    per-host config

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+(defun init/hashed-host-slug ()
+  "Return a hashed version of the top-level domain name"
+  (md5 (concat "UGXp4Adb.p8m;baTN8ybKxebV"
+               (car (split-string (downcase (system-name)) "\\.")))))
+
 (defun dotspacemacs/layers ()
   (setq-default dotspacemacs-distribution 'spacemacs
                 dotspacemacs-configuration-layer-path '()
@@ -27,14 +32,29 @@
                                       leuven
                                       monokai
                                       zenburn)
-                dotspacemacs-default-font '("Menlo"
-                                            :size 17
-                                            :weight normal
-                                            :width normal
-                                            :powerline-scale 1.1)
                 dotspacemacs-leader-key "SPC"
                 dotspacemacs-emacs-leader-key "M-m"
-                dotspacemacs-command-key ":"))
+                dotspacemacs-command-key ":")
+  (let ((host (init/hashed-host-slug))
+        (desktop "24e0c4b6b602908fd5cc6be519f8d96b")
+        (laptop "1808cc85340608cde18d5cab8b3be29d"))
+    (message (concat "init/hashed-host-slug=" host))
+    (when (equal host desktop)
+      (progn
+        (when window-system (set-frame-size (selected-frame) 100 60))
+        (setq-default dotspacemacs-default-font '("Menlo"
+                                                  :size 17
+                                                  :weight normal
+                                                  :width normal
+                                                  :powerline-scale 1.1))))
+    (when (equal host laptop)
+      (progn
+        (when window-system (set-frame-size (selected-frame) 120 46))
+        (setq-default dotspacemacs-default-font '("Menlo"
+                                                  :size 13
+                                                  :weight normal
+                                                  :width normal
+                                                  :powerline-scale 1.1))))))
 
 (defun dotspacemacs/user-init ()
   (setq deft-directory "/Users/sam/Dropbox/Notes")
