adcfda72a8a71318f6a7ffc31102dcf42d14a37c
Sam Doshi <sam@metal-fish.co.uk>
2016-02-15 09:55:09 +0000

    add reset-master.sh and reset-develop.sh

---
+++ reset-develop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf ~/.emacs.d/
+git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
---
+++ reset-master.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf ~/.emacs.d/
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
