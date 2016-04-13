fbb963ee764a8674ed930c8b508ddde5d3a1d7a7
Sam Doshi <sam@metal-fish.co.uk>
2016-04-13 14:11:04 +0100

    disable paste-micro-state

--- init.el
+++ init.el
@@ -63,6 +63,7 @@
                 dotspacemacs-emacs-leader-key "M-m"
                 dotspacemacs-command-key ":"
                 dotspacemacs-scratch-mode 'emacs-lisp-mode
+                dotspacemacs-enable-paste-micro-state nil
                 dotspacemacs-check-for-update nil)
   (setq-default spacemacs-evil-cursors `((normal       ,init/solarized-yellow   box)
                                          (insert       ,init/solarized-green   (bar  . 2))
